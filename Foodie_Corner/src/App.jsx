2fef79cebabce3b3f1cf21cd1d448cf42d674551
rrabia133-sketch <rrabia133@gmail.com>
2025-09-19 12:43:01 +0800

    install styled-components

--- Foodie_Corner/src/App.jsx
+++ Foodie_Corner/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { styled } from "styled-components";
 
+import logo from "./images/logo.png";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MainContainer>
+        <TopContainer>
+          <div className="logo">
+            <img src={logo} alt="logo"></img>
+          </div>
+          <div className="search">
+            <input placeholder="search Here"></input>
+          </div>
+        </TopContainer>
+      </MainContainer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+const MainContainer = styled.div`
+  width: 1440;
+  height: 241;
+  background: rgba(50, 51, 52, 1);
+`;
+const TopContainer = styled.section``;
--- Foodie_Corner/src/main.jsx
+++ Foodie_Corner/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import './index.css'
+import App from "./App.jsx";
+import { createGlobalStyle } from "styled-components";
 
-createRoot(document.getElementById('root')).render(
+const GlobalStyle = createGlobalStyle`
+*{
+  box-sizing: border-box;
+  margin:0 ;
+  padding: 0;
+}
+`;
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <GlobalStyle />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
